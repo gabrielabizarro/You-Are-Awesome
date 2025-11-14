270346fdd654a16fb536ddb5e8b9efaa37f05982
gabrielabizarro <gbizarro2027@gmail.com>
2025-11-14 12:47:14 -0800

    Initial Commit

---
+++ You Are Awesome/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  You Are Awesome
+//
+//  Created by BIZARRO, GABRIELA on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = "I Am A Programmer"
+    
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great") {
+                message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+ 
---
+++ You Are Awesome/You_Are_AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_AwesomeApp.swift
+//  You Are Awesome
+//
+//  Created by BIZARRO, GABRIELA on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
