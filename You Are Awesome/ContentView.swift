9ee9b5ade5f250fdec8d149a9f91221689e3a730
gabrielabizarro <gbizarro2027@gmail.com>
2025-12-01 13:02:24 -0800

    12/01

--- You Are Awesome/ContentView.swift
+++ You Are Awesome/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
+    @State private var messageNumber = 0
     @State private var imageName = ""
+    @State private var imageNumber = 0
     var body: some View {
         
         VStack {
@@ -24,16 +26,32 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
+            
             Button("Press Me") {
-                let message1 = "You are awesome"
-                let message2 = "You are great"
+                let messages = ["I Love You",
+                                "I Love Myself",
+                                "I Love Us",
+                                "I Love Today",
+                                "I Love This Image "]
                 
-                message = (message == message1 ? message2 : message1)
-                imageName = (imageName == "image0" ? "image1" : "image0")
-            }
+                message = messages[messageNumber]
+                messageNumber += 1
+                
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
+                imageName = "image\(imageNumber)"
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber=0
+                }            }
+            
             
             .buttonStyle(.borderedProminent)
             .font(.title2)
@@ -46,4 +64,5 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
 
